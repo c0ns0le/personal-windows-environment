8096e933050079b7117a8b82f5d6f5af5383e0db
Ole Morten Didriksen <code@oledid.com>
2016-04-09 21:51:09 +0200

    Try to fix error

--- main.ps1
+++ main.ps1
@@ -1,7 +1,1 @@
-$desktopPath = [Environment]::GetFolderPath('Desktop')
-Set-Location $desktopPath
-New-Item -ItemType Directory -Force -Path Downloads
-Set-Location Downloads
-Write-Host Downloading script...
-Invoke-WebRequest https://raw.githubusercontent.com/oledid/personal-windows-environment/master/run-as-elevated.ps1 -OutFile script.ps1
-Start-Process powershell -verb runas ./script.ps1
+$desktopPath = [Environment]::GetFolderPath('Desktop'); Set-Location $desktopPath; New-Item -ItemType Directory -Force -Path Downloads; Set-Location Downloads; Write-Host Downloading script...; Invoke-WebRequest https://raw.githubusercontent.com/oledid/personal-windows-environment/master/run-as-elevated.ps1 -OutFile script.ps1; Start-Process powershell -verb runas ./script.ps1
