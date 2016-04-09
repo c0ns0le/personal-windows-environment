72eee87e763fec1560fa126c60d0b2da2e4d202b
Ole Morten Didriksen <code@oledid.com>
2016-04-09 22:18:17 +0200

    Try to fix run error

--- main.ps1
+++ main.ps1
@@ -4,4 +4,6 @@ New-Item -ItemType Directory -Force -Path Downloads
 Set-Location Downloads
 Write-Host Downloading script...
 Invoke-WebRequest https://raw.githubusercontent.com/oledid/personal-windows-environment/master/run-as-elevated.ps1 -OutFile script.ps1
-Start-Process powershell -verb runas ./script.ps1
+Write-Host Running script as administrator...
+$scriptPath = Join-Path $pwd script.ps1
+Start-Process powershell -verb runas $scriptPath
