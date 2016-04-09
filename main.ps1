07dae7beda38d1c2a5d5592f4e3ea33c86928e22
Ole Morten Didriksen <code@oledid.com>
2016-04-09 21:46:17 +0200

    Try to fix error

--- main.ps1
+++ main.ps1
@@ -3,5 +3,5 @@ cd $desktopPath
 New-Item -ItemType Directory -Force -Path Downloads
 cd Downloads
 Write-Host Downloading script...
-wget https://raw.githubusercontent.com/oledid/personal-windows-environment/master/run-as-elevated.ps1 -OutFile script.ps1
-start-process powershell -verb runas ./script.ps1
+Invoke-WebRequest https://raw.githubusercontent.com/oledid/personal-windows-environment/master/run-as-elevated.ps1 -OutFile script.ps1
+Start-Process powershell -verb runas ./script.ps1
